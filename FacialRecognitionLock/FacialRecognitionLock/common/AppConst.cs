046ede6e6558e8c5d2150307742548883f645e3e
MinKKKKKK <343733617@qq.com>
2019-04-10 11:42:20 +0800

    app控制脚本

---
+++ FacialRecognitionLock/FacialRecognitionLock/common/AppConst.cs
@@ -0,0 +1,27 @@
+﻿namespace FacialRecognitionLock
+{
+    /// <summary>
+    /// app控制台 (初次开启之前,请设置好你的百度人脸识别控制台里的APP_ID、API_KEY、SECRET_KEY)
+    /// </summary>
+    public class AppConst
+    {
+        public static bool isDebugMode = true;                                           // 是否使用Demo测试模式
+        public static bool isOpenLock = false;                                           // 是否使用控制器
+        public static bool isOpenCamera = true;                                          // 是否使用摄像头
+        public static bool isOpenLocalServer = false;                                    // 是否使用本地ip服务器
+        public static bool isOpenLocalLock = false;                                      // 是否使用本地ip门锁控制器
+
+        public static string localServerIP = "192.168.1.108";                            // RPI3B的IP地址(本地测试)
+        public const string serverPort = "60000";                                        // 本地服务器默认缺省端口
+        public const int lockPort = 60000;                                               // lock主控制器默认缺省端口
+
+        public static double repeatActionDelta = 5;                                      // 重复执行方法延迟
+        public static double lockOpenningDelay = 5;                                      // 开门延迟
+
+        /// Baidu sdk配置 (通过ak、sk换取AI调用web接口)
+        public const string APP_ID = "15700137";                                         // 暂时无用.
+        public static string API_KEY = "xNEfNoAZCr78tX2Wv4WfeRDy";                       // AK
+        public static string SECRET_KEY = "V0vM0hhw0n4hcVVGFvGYMSBVzHr5klDx";            // SK
+        public static string HaarCascadeAlt = @"data/haarcascade_frontalface_alt.xml";   // HaarCascadeAlt算法人脸检测xml配置
+    }
+}
